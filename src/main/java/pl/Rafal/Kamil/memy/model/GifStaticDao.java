1d032d95de738a9ef5073986b18f5c92b75d2eff
laptopKamilLinux <k.figarski@interia.pl>
2018-08-01 12:35:26 +0200

    serch added

--- src/main/java/pl/Rafal/Kamil/memy/model/GifStaticDao.java
+++ src/main/java/pl/Rafal/Kamil/memy/model/GifStaticDao.java
@@ -13,12 +13,12 @@ public class GifStaticDao implements GifDao {
 
     static {
 
-        gifs.add( new Gif(1, "android-explosion",true));
-        gifs.add(new Gif (2, "ben-and-mike"));
-        gifs.add(new Gif (3, "book-dominos"));
-        gifs.add(new Gif (4, "compiler-bot",true));
-        gifs.add(new Gif (5, "cowboy-coder"));
-        gifs.add(new Gif (6, "infinite-andrew",true));
+        gifs.add(new Gif(1, "android-explosion", true));
+        gifs.add(new Gif(2, "ben-and-mike"));
+        gifs.add(new Gif(3, "book-dominos"));
+        gifs.add(new Gif(4, "compiler-bot", true));
+        gifs.add(new Gif(5, "cowboy-coder"));
+        gifs.add(new Gif(6, "infinite-andrew", true));
 
     }
 
@@ -27,11 +27,11 @@ public class GifStaticDao implements GifDao {
         return gifs;
     }
 
-    public List<Gif> findAllFavorites(){
+    public List<Gif> findAllFavorites() {
 
         favorites = new ArrayList<>();
-        for (Gif gif : gifs){
-            if ( gif.isFavorite()){
+        for (Gif gif : gifs) {
+            if (gif.isFavorite()) {
                 favorites.add(gif);
             }
         }
@@ -39,6 +39,28 @@ public class GifStaticDao implements GifDao {
     }
 
 
+    public String szukaj(String q) {
+        String wynik;
+        wynik = "";
+        for (Gif gif : gifs) {
+            if (gif.getName().equals(q)) {
+               // wynik = "gifs/" + gif.getName() + ".gif";
+                wynik = "Sukces Jest Taki Memes!";
+            } else wynik = "Nie Ma takiego Gifsa - Smuteczkowo :( ";
+        }
+        return wynik;
+    }
 
+    //@Override
+    public List<Gif> find(String q) {
+        List<Gif> search = new ArrayList<>();
+        for (Gif gif : gifs) {
+            if (gif.getName().contains(q)) {
+                search.add(gif);
+            }
+        }
+        return search;
+    }
 
 }
+
